a857a7f400d767e684983a3b7708be5708900bb0
SamHMews <samueljosephhowes@gmail.com>
2026-09-25 15:50:44 +0100

    Simplify Lower exercise names

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='training-v18';
+const CACHE='training-v19';
 const ASSETS=['./','./index.html','./style.css','./mobile.css','./gestures.js','./reps.js','./feedback.css','./stack.css','./app.js','./celebration.js','./weight-plate.png','./github-sync.js','./model.js','./storage.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 let legacyUpdateClient=null;
 self.addEventListener('install',event=>event.waitUntil((async()=>{
